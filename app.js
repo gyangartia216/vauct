cd9b727dfe2cae2219d8a5472633de942886c153
gyangartia216 <96109912+gyangartia216@users.noreply.github.com>
2022-05-14 00:01:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,12 +10,11 @@ const MYSQLStore = require("express-mysql-session")(session);
 const cors = require("cors");
 const bcrypt = require('bcrypt');
 const req = require('express/lib/request');
+var app = express();
 
 var indexRouter = require('./routes/index');
-//var indexRouter = require('./app.js');
+// var indexRouter = require('./app.js');
 var usersRouter = require('./routes/users');
-
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -29,31 +28,54 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 
 app.listen('5001', ()=> {
   console.log("Server started at 5001");
 })
 
+
+app.use(parser.json());
+app.use(express.urlencoded());
+app.use(express.static('public'));
+app.use(express.static(__dirname+'/css'));
+app.use(parser.urlencoded( { extended: true }));
+app.use(cookieParser())
+
+//database connection
+var connection=mysql.createConnection(
+    {
+        host:'localhost',
+        user:'root',
+        password:'gyan05',
+        database:'vauct'
+    });
+ 
+    connection.connect((err)=>
+    {
+        if(!err)
+            console.log('DB Connected...');
+        else
+            console.log('Error');
+    })
 
 // ******************************//
 
@@ -63,11 +85,11 @@ app.use(express.urlencoded());
 //app.use(express.static('public'));
 app.use(express.static(__dirname+'/css'));
 
-app.use(cors({
-	origin: ["http://localhost:5001"],
-	methods: ["GET", "POST"],
-	credentials: true
-}));
+// app.use(cors({
+// 	origin: ["http://localhost:5001"],
+// 	methods: ["GET", "POST"],
+// 	credentials: true
+// }));
 
 const saltRounds = 10
 
@@ -86,25 +108,32 @@ app.use(session({
 );
 
   
-app.get("/header", (req, res) =>{
-        res.render("header2.ejs");
-      })
 
      app.get('/register',(req, res)=>
      {
          //console.log(req.url);
-         res.sendFile(__dirname +'views/register24.ejs');
+         res.render('register24.ejs');
      });
 
      app.get('/login', (req, res)=>
      {
-         res.sendFile(__dirname +'views/login24.ejs');
+         res.render('login24.ejs');
      });
+
+       app.get("/about", (req, res) =>{
+      res.render("about.ejs");
+    });
+  
+    app.get("/contact", (req, res) =>{
+      res.render("contact.ejs");
+    });
 
     //  app.get('/home',(req,res)=> {
     //     res.sendFile(__dirname +'index.ejs');
     //     //res.sendFile("/index.html");
     // });
+
+
 
 
     app.post('signup',(req,res)=>
@@ -128,14 +157,14 @@ app.get("/header", (req, res) =>{
             })
     });
 
-app.get("/login", (req, res) =>{
-    if(req.session.user )
-    {
-            res.send({ loggedIn: true, user:req.session.user});
-    }else{
-        res.send({loggedIn: false});
-    }
-})
+// app.get("/login", (req, res) =>{
+//     if(req.session.user )
+//     {
+//             res.send({ loggedIn: true, user:req.session.user});
+//     }else{
+//         res.send({loggedIn: false});
+//     }
+// })
 
 //Logout function
 app.get('/logout',function(req,res)
@@ -144,6 +173,7 @@ app.get('/logout',function(req,res)
   req.session.destroy();
   res.redirect('/');
 });
+
 
 app.post('/signin', (req, res) => 
 {
@@ -160,16 +190,31 @@ app.post('/signin', (req, res) =>
             if(result.length > 0){
                 bcrypt.compare(password, result[0].password, (error, response)=>{
                     if(response){
-                        let user={
-                            loggedin:true,
-                            name: result[0].name
-                        }
-                        res.cookie("userData", user);
                         req.session.user = result[0].name;
+                       // localStorage.setItem(req.session.user, result[0].name);
+                        sessionStorage.setItem(req.session.user, result[0].name);
+                        sessionStorage.setItem(req.session.loggedin, true);
+                        essionStorage.setItem(req.session.email, email);
                         console.log("Hiii "+ req.session.user);
                         console.log(result);
-                        res.redirect('home#login=true');
-                         //res.send(result);
+                        onsole.log(req.session.loggedin);
+                        console.log("Logged in successfully!!");
+                        //req.session.loggedin=true;
+                    //     let user={
+                    //         loggedin:true,
+                    //         name: result[0].name
+                    //     }
+                    //     res.cookie("userData", user);
+                    //   console.log(req.cookies);
+                      // req.session.e = e;
+                      req.session.save();
+
+                        // Redirect to home page
+                         
+                        //  req.session.user = result[0].name;
+                         req.session.email = email;
+                         //res.redirect('/#login=true');		
+
                          
                     }
                     else{
@@ -186,5 +231,9 @@ app.post('/signin', (req, res) =>
     );
 })
 
+
+// app.get('/', function(req, res, next) {
+//     res.render('index', { title: 'Express' ,loggedin:req.session.loggedin});
+//   });
 
 module.exports = app;
